fc0602425219cc5771a0792cac7b39dfab20f7a9
Quickz <alexserg@inbox.lv>
2021-10-15 21:24:08 +0300

    added a couple of extension methods for converting a color to a hexadecimal string

--- Scripts/Runtime/ColorExtensions.cs
+++ Scripts/Runtime/ColorExtensions.cs
@@ -18,5 +18,21 @@ namespace UDK
         {
             return new Color(1f - source.r, 1f - source.g, 1f - source.b, source.a);
         }
+
+        /// <summary>
+        ///  <b>Example:</b> AABBCC
+        /// </summary>
+        public static string AsHexString(this Color source)
+        {
+            return ColorUtility.ToHtmlStringRGB(source);
+        }
+
+        /// <summary>
+        ///  <b>Example:</b> AABBCCFF
+        /// </summary>
+        public static string AsHexStringAlpha(this Color source)
+        {
+            return ColorUtility.ToHtmlStringRGBA(source);
+        }
     }
 }
