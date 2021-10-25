1145617296441766063992bc914dc8cffd1992f0
Miguel Medina Ballesteros <miguel.medina-ballesteros@ubisoft.com>
2021-10-25 01:39:56 +0100

    Add Vector3 shortcuts to floatext

--- Scripts/Runtime/Extensions/FloatExt.cs
+++ Scripts/Runtime/Extensions/FloatExt.cs
@@ -27,5 +27,12 @@ namespace UDK
 				return diff / (absA + absB) < epsilon;
 			}
 		}
+
+		public static Vector3 Up(this float self) => self * Vector3.up;
+		public static Vector3 Down(this float self) => self * Vector3.down;
+		public static Vector3 Right(this float self) => self * Vector3.right;
+		public static Vector3 Left(this float self) => self * Vector3.left;
+		public static Vector3 Forward(this float self) => self * Vector3.forward;
+		public static Vector3 Back(this float self) => self * Vector3.back;
 	}
 }
