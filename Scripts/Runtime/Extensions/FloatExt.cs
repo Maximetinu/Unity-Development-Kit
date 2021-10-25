8db8c7cfb9549ee9eae5c8c73bc25ac00226523d
Miguel Medina Ballesteros <miguel.medina-ballesteros@ubisoft.com>
2021-10-25 01:36:57 +0100

    Add float ext

---
+++ Scripts/Runtime/Extensions/FloatExt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace UDK
+{
+	public static class FloatExt
+	{
+		public static bool NearlyEquals(this float a, float b, float epsilon)
+		{
+			float absA = Mathf.Abs(a);
+			float absB = Mathf.Abs(b);
+			float diff = Mathf.Abs(a - b);
+
+			if (a == b)
+			{
+				// shortcut, handles infinities
+				return true;
+			}
+			else if (a == 0 || b == 0 || diff < float.MinValue)
+			{
+				// a or b is zero or both are extremely close to it
+				// relative error is less meaningful here
+				return diff < (epsilon * float.MinValue);
+			}
+			else
+			{
+				// use relative error
+				return diff / (absA + absB) < epsilon;
+			}
+		}
+	}
+}
