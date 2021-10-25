61665c1feb480d73667ab2deca5fae5c6457b878
Miguel Medina Ballesteros <miguel.medina-ballesteros@ubisoft.com>
2021-10-25 01:37:14 +0100

    Add quaternion ext

---
+++ Scripts/Runtime/Extensions/QuaternionExt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UDK
+{
+	public static class QuaternionExt
+	{
+		public static Quaternion With(this Quaternion original, float? x = null, float? y = null, float? z = null)
+		{
+			return Quaternion.Euler(x ?? original.eulerAngles.x, y ?? original.eulerAngles.y, z ?? original.eulerAngles.z);
+		}
+	}
+}
