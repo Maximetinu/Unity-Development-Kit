367caa3e5b47ee41c453feacf3b3e062f9c71b80
Miguel Medina Ballesteros <miguel.medina-ballesteros@ubisoft.com>
2021-10-25 01:37:06 +0100

    Add game object exts

---
+++ Scripts/Runtime/Extensions/GameObjectExt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UDK
+{
+	public static class GameObjectExt
+	{
+		public static void SetActive(this GameObject[] objs, bool active)
+		{
+			foreach (GameObject obj in objs) obj?.SetActive(active);
+		}
+	}
+}
